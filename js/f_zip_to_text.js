1809dcef32d5371da61389b72b3a1f5249e01613
toyotamakenkyusyo <keiobe2016@yahoo.co.jp>
2019-10-10 23:02:55 +0900

    Zlibの扱い方を変更

--- js/f_zip_to_text.js
+++ js/f_zip_to_text.js
@@ -1,9 +1,9 @@
-//https://cdn.jsdelivr.net/npm/zlibjs@0.3.1/bin/unzip.min.jsが必要
 //a_xhr_getはf_xhr_get.jsのf_xhr_getを入れる
-export async function f_zip_to_text(a_url, a_xhr_get) {
+//a_zlibはhttps://cdn.jsdelivr.net/npm/zlibjs@0.3.1/bin/unzip.min.jsのZlibを入れる
+export async function f_zip_to_text(a_url, a_xhr_get, a_zlib) {
 	const c_array = (await a_xhr_get(a_url, "arraybuffer")).response;
 	const c_byte = new Uint8Array(c_array);
-	const c_unzip = new Zlib.Unzip(c_byte);
+	const c_unzip = new a_zlib.Unzip(c_byte);
 	const c_filenames = c_unzip.getFilenames();
 	const c_plain = c_unzip.decompress(c_filenames[0]);
 	const c_files = {};
